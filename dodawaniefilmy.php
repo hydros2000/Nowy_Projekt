14901a1abb4755381f7b1e6d775f7aec6eb83121
hydros2000 <bartekkalmar@gmail.com>
2019-05-17 09:16:08 +0200

    17.05.2019

--- dodawaniefilmy.php
+++ dodawaniefilmy.php
@@ -1,10 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
 <?php
 //session_start();
 require_once "connectfilmy.php";
-$host = "192.168.122.203";
-$dbusername = "root";
-$dbpassw0rd = "";
-$dbname = "filmy";
+#$host = "192.168.122.203";
+#$dbusername = "root";
+#$dbpassword = "";
+#$dbname = "filmy";
 $idfilmu = filter_input(INPUT_POST,'id');
 $tytul = filter_input(INPUT_POST,'tytul');
 $rokprodukcji = filter_input(INPUT_POST,'rok');
@@ -15,10 +26,10 @@ if (!empty($tytul)){
 if (!empty($rokprodukcji)){
 if (!empty($gatunek)){
 if (!empty($opis)){
-#$host = "192.168.122.203";
-#$dbusername = "root";
-#$dbpassw0rd = "";
-#$dbname = "filmy";
+$host = "192.168.122.203";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "filmy";
 $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
 if(mysqli_connect_error())
 {
@@ -59,12 +70,44 @@ echo "That should not be empty";
 die();
 }
 }
-$con = mysql_connect($host,$dbusername,$dbpassword);
-$result = mysql_query("SELECT * FROM film");
+#connect();
+#$con = mysql_connect($host,$dbusername,$dbpassword);
 
-mysql_select_db("filmy", $con);
-echo '<table><tr><th>id_filmu</th><th>tytul</th><th>rok_produkcji</th><th>gatunek</th><th>opis</th></tr>';
-	while($row = mysql_fetch_array($result)) {
-		echo "<tr><td>{$row['id']}</td><td>{$row['tytul']}</td><td>{$row['rok']}</td><td>{$row['gatunek']}</td><td>{$row['opis']}</td></tr>";
-	}
-	echo '</table>';
+#mysql_select_db($dbname, $polaczenie);
+
+#$result = mysql_query("SELECT * FROM film");
+
+
+#echo '<table><tr><th>id_filmu</th><th>tytul</th><th>rok_produkcji</th><th>gatunek</th><th>opis</th></tr>';
+#	while($row = mysql_fetch_array($result)) {
+#		echo "<tr><td>{$row['id']}</td><td>{$row['tytul']}</td><td>{$row['rok']}</td><td>{$row['gatunek']}</td><td>{$row['opis']}</td></tr>";
+#	}
+#	echo '</table>';
+$host = "192.168.122.203";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "filmy";
+
+$conn2 = new mysqli($host, $dbusername, $dbpassword, $dbname);
+// Check connection
+if ($conn2->connect_error) {
+    die("Connection failed: " . $conn2->connect_error);
+} 
+$sql2 = "SELECT * FROM film";
+$result = $conn2->query($sql2);
+
+if ($result->num_rows > 0) {
+    echo '<table><tr><th>id_filmu</th><th>tytul</th><th>rok_produkcji</th><th>gatunek</th><th>opis</th></tr>';
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>{$row['id_filmu']}</td><td>{$row['tytul']}</td><td>{$row['rok_produkcji']}</td><td>{$row['gatunek']}</td><td>{$row['opis']}</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn2->close();
+?>
+</body>
+</html>
--- infofilmy.php
+++ infofilmy.php
@@ -7,10 +7,18 @@
 	<meta charset="utf-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome-1"/>
 	<title>Strona logowania</title>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+label {
+position:relative;
+
+}
+</style>
 </head>
 
 <body>
-
 <?php
 	echo "<p>Witaj ".$_SESSION['username']."!";
 	
@@ -19,12 +27,42 @@
 	<form action="dodawaniefilmy.php" method="post">
 	<br>
 	<h1>Dodawanie rekordow</h1>
-	Dodaj Id filmu:<input type="text" name="id"/><br>
-	Dodaj tytul filmu:<input type="text" name="tytul"/><br>
-	Dodaj rok produkcji filmu:<input type="text" name="rok"/><br>
-	Dodaj gatunek filmu:<input type="text" name="gatunek"/><br>
-	Dodaj opis do filmu:<input type="text" name="opis"/><br>
+	<label>Id filmu:</label><input type="text" name="id"/><br>
+	<label>Tytul filmu:</label><input type="text" name="tytul"/><br>
+	<label>Rok produkcji filmu:</label><input type="text" name="rok"/><br>
+	<label>Gatunek filmu:</label><input type="text" name="gatunek"/><br>
+	<label>Opis do filmu:</label><input type="text" name="opis"/><br>
 	<input type="submit" value="Dodaj rekord"/>
 	</form>
+<br>
+<?php
+
+$host = "192.168.122.203";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "filmy";
+
+$conn2 = new mysqli($host, $dbusername, $dbpassword, $dbname);
+// Check connection
+if ($conn2->connect_error) {
+    die("Connection failed: " . $conn2->connect_error);
+}
+$sql2 = "SELECT * FROM film";
+$result = $conn2->query($sql2);
+
+if ($result->num_rows > 0) {
+    echo '<table><tr><th>id_filmu</th><th>tytul</th><th>rok_produkcji</th><th>gatunek</th><th>opis</th></tr>';
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>{$row['id_filmu']}</td><td>{$row['tytul']}</td><td>{$row['rok_produkcji']}</td><td>{$row['gatunek']}</td><td>{$row['opis']}</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn2->close();
+?>
+
 </body>
 </html>
